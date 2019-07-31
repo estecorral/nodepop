e5562c3cca5b2ff934438974ceacd0ccdafba6ef
Esteban <estecorral@gmail.com>
2019-07-31 19:45:12 +0200

    Listado inicial de anuncios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const Anuncio = require('../models/Anuncio');
+const { query, validationResult } = require('express-validator');
+
+/* GET home page.*/
+router.get('/', async (req, res, next) => {
+    try {
+        res.locals.anuncios = await Anuncio.find().exec();
+        console.log(res.locals.anuncios);
+        res.render('index');
+    }catch (e) {
+        next(e);
+    }
 });
 
+router.get('/', (req, res, next) => {
+    console.log('req.query', req.query);
+    res.send('ok');
+});
 module.exports = router;
